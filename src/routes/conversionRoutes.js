8f677197ad5101370560a97fa062c80a234e10c4
Pushpak003 <pushpakpathe63@gmail.com>
2025-07-29 00:49:24 +0530

    Adds media file conversion endpoint

--- src/routes/conversionRoutes.js
+++ src/routes/conversionRoutes.js
@@ -3,9 +3,10 @@ import {authMiddleware} from "../middlewares/authMiddleware.js";
 import {upload} from "../middlewares/uploadMiddleware.js";
 import { convertImage } from "../controllers/imageconversionController.js";
 import {documentConversionController} from "../controllers/documentController.js";
+import { mediaConversionController } from "../controllers/audiovideoController.js";
 const router = express.Router();
 
 router.post("/image",authMiddleware,upload.single("file"),convertImage);
 router.post("/document",authMiddleware, upload.single("file"), documentConversionController);
-
+router.post("/media",authMiddleware, upload.single("file"),mediaConversionController);
 export default router;
