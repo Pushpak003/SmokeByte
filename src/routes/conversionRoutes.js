1abe1acf88749b465d37902b1da37f5f8b82d781
Pushpak003 <pushpakpathe63@gmail.com>
2025-07-28 14:52:22 +0530

    Added Document conversion route

--- src/routes/conversionRoutes.js
+++ src/routes/conversionRoutes.js
@@ -2,9 +2,10 @@ import express from "express";
 import {authMiddleware} from "../middlewares/authMiddleware.js";
 import {upload} from "../middlewares/uploadMiddleware.js";
 import { convertImage } from "../controllers/imageconversionController.js";
-
+import {documentConversionController} from "../controllers/documentController.js";
 const router = express.Router();
 
 router.post("/image",authMiddleware,upload.single("file"),convertImage);
+router.post("/document",authMiddleware, upload.single("file"), documentConversionController);
 
 export default router;
