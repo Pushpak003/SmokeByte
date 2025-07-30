889b89bf3852770258c484317e822fc747116e5e
Pushpak003 <pushpakpathe63@gmail.com>
2025-07-31 00:24:06 +0530

    added supabase for converted files

--- src/services/audiovideoService.js
+++ src/services/audiovideoService.js
@@ -3,12 +3,10 @@ import path from 'path';
 
 export const convertMedia = async (inputPath, outputFormat) =>{
     return new Promise((resolve, reject) => {
-        const outputName = `converted-${Date.now()}.${outputFormat}`;
-        const outputPath = path.join("public/uploads", outputName);
-
+        const outputPath = path.join("public/uploads", `converted-${Date.now()}.${outputFormat}`);
         ffmpeg(inputPath)
           .toFormat(outputFormat)
-          .on("end",()=> resolve({ outputName}))
+          .on("end",()=> resolve({ outputPath}))
           .on("error",reject)
           .save(outputPath);
 
