29b5a32902eda82359aba19cd238625b36272249
Pushpak003 <pushpakpathe63@gmail.com>
2025-08-02 01:55:13 +0530

    added Queue system using RabbitMQ and Redis

--- src/controllers/documentController.js
+++ src/controllers/documentController.js
@@ -1,48 +1,29 @@
-import fs from 'fs';
-import path from 'path';
-import File from '../models/fileModel.js';
-import ConversionLog from '../models/conversionLogs.js';
-import { convertDocument } from '../services/documentService.js';
-import { uploadFileToSupabase } from '../services/storageService.js';
+import { addToQueue } from "../jobs/conversionQueue.js";
 
-export const documentConversionController = async(req, res) => {
-    try{
-        const userId = req.user.id;
-        const targetFormat = req.body.targetFormat?.toLowerCase();
-        const allowedFormats =["pdf", "txt", "odt", "doc", "docx", "html"];
-        const inputPath = req.file.path;
-        if(!allowedFormats.includes(targetFormat)){
-            return res.status(400).json({message:"Invalid target Format"});
-        }
 
-        const {convertedPath} = await convertDocument(inputPath, targetFormat);
-        
+export const documentConversionController = async (req, res) => {
+  try {
+   
+    const targetFormat = req.body.targetFormat?.toLowerCase();
+    const allowedFormats = ["pdf", "txt", "odt", "doc", "docx", "html"];
+    if (!allowedFormats.includes(targetFormat)) {
+      return res.status(400).json({ message: "Invalid target format" });
+    }
+   const job = await addToQueue({
+  filePath: req.file.path,
+  targetFormat,
+  userId: req.user.id,
+  originalName: req.file.originalname,
+  fileType: req.file.mimetype,
+  mimeType: req.file.mimetype,
+  fileSize: req.file.size,
+});
 
-        const fileName = `${Date.now()}-${req.file.originalname.split('.')[0]}.${targetFormat}`;
-
-        const fileUrl = await uploadFileToSupabase(convertedPath, fileName);
-        
-        const fileRecord = await File.create({
-            filename: req.file.originalname,
-            filetype: req.file.mimetype,
-            filesize: req.file.size,
-            user_id: userId,
-            converted_file_url:fileUrl
-        });
-        await ConversionLog.create({
-            file_id: fileRecord.id,
-            target_format:targetFormat,
-            status:"completed",
-            converted_file_url:fileUrl
-        });
-
-        fs.unlinkSync(req.file.path);
-
-        res.json({message:"Document Converted Successfully", url:fileUrl});
-        } catch(err) {
-            console.error(err);
-            res.status(500).json({
-                message:"Document Conversion Failed"
-            });
-        }
-    };
+    res.status(200).json({
+      message: "Document conversion job added to queue",
+      jobId: job.id,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
