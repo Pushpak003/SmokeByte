e63432e0bacf7d21915d9e9b246ce151f25e53f8
Pushpak003 <pushpakpathe63@gmail.com>
2025-08-02 01:55:43 +0530

    added Queue system using RabbitMQ and Redis

--- src/controllers/imageconversionController.js
+++ src/controllers/imageconversionController.js
@@ -1,48 +1,28 @@
-import fs from 'fs';
-import File from '../models/fileModel.js'; // Assuming you have a File model for storing file metadata
-import ConversionLog from '../models/conversionLogs.js';
-import { convertImageFile } from '../services/imageService.js';
-import { uploadFileToSupabase } from '../services/storageService.js';
-export const convertImage = async(req, res) =>{
-    try{
-        const userID = req.user.id;
-        const filePath = req.file.path;
-        const targetFormat = req.body.targetFormat?.toLowerCase();
-
-      
-        const convertedFileurl = await convertImageFile(filePath,targetFormat);
-        console.log("Converted local file path:", convertedFileurl);
+import { addToQueue } from "../jobs/conversionQueue.js";
 
 
-        const fileName = `${Date.now()}-${req.file.originalname.split('.')[0]}.${targetFormat}`;
-        const fileUrl = await uploadFileToSupabase(convertedFileurl, fileName);
-       console.log("File uploaded to Supabase at:", fileUrl);
-       
-       if(!fileUrl){
-    throw new Error("Uploaded file URL is null or undefined");
-}
-        const fileRecord = await File.create({
-            filename: req.file.originalname,
-            filetype: req.file.mimetype,
-            filesize:req.file.size,
-            user_id: userID,
-            converted_file_url:fileUrl
-        });
-        await ConversionLog.create({
-            file_id: fileRecord.id,
-            target_format: targetFormat,
-            status:"completed",
-            converted_file_url: fileUrl
-        });
-
-        fs.unlinkSync(convertedFileurl); // Clean up the local file after upload
-
-        res.status(200).json({
-           message: "Image converted successfully",
-           fileUrl: fileUrl
-});
-
-    }catch(err) {
-        res.status(500).json({message: err.message});
+export const convertImage = async (req, res) => {
+  try {
+    const targetFormat = req.body.targetFormat?.toLowerCase();
+    const allowedFormats = ["png", "jpeg", "jpg", "webp"];
+    if (!allowedFormats.includes(targetFormat)) {
+      return res.status(400).json({ message: "Invalid target format" });
     }
+
+    const job = await addToQueue({
+  filePath: req.file.path,
+  targetFormat,
+  userId: req.user.id,
+  originalName: req.file.originalname,
+  fileType: req.file.mimetype,
+  mimeType: req.file.mimetype,
+  fileSize: req.file.size,
+});
+   res.status(200).json({
+      message: "Image conversion job added to queue",
+      jobId: job.id,
+    });
+   }catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
