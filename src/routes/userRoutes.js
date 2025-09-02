ec3a5ca72577e970901a0feb7e23f05637d4bec2
Pushpak003 <pushpakpathe63@gmail.com>
2025-09-02 23:30:33 +0530

    Added user/me route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import { authMiddleware } from './../middlewares/authMiddleware.js';
 import { getUserHistory } from '../controllers/userHistorycontroller.js';
-
+import {getUserProfile} from "../controllers/userHistorycontroller.js"
 const router = express.Router();
+const noCache = (req, res, next) => {
+  res.set('Cache-Control', 'no-store');
+  next();
+};
 
-router.get('/history',authMiddleware, getUserHistory); 
+router.get('/history',authMiddleware, getUserHistory);
+router.get('/me',noCache, authMiddleware, getUserProfile);
 
 export default router;
