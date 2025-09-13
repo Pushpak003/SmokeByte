16da2dd680ecc38553ef4d8b13b28ee6535f5db0
Pushpak003 <pushpakpathe63@gmail.com>
2025-09-13 22:57:57 +0530

    added ssl for render

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-  logging: console.log 
-    
+  logging: console.log,
+    dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Render ke self-signed cert ke liye
+    },
+  },
 });
 export default sequelize;
