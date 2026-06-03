2236d7a65caf0e6349c0d48050fefb3a365d43bb
Pushpak003 <pushpakpathe63@gmail.com>
2026-06-03 12:08:34 +0530

    chore: integrate helmet and error handling middleware in app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,22 @@
 import express from "express";
 import path from "path";
 import cors from "cors";
+import helmet from "helmet";
 import statusRoutes from "./routes/statusRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
 import conversionRoutes from "./routes/conversionRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import downloadRoutes from "./routes/downloadRoutes.js"
 import limiter from "./middlewares/rateLimit.js";
+import { errorHandler } from "./middlewares/errorMiddleware.js";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
 
 app.set("trust proxy", 1);
+
+app.use(helmet());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,4 +29,5 @@ app.use("/uploads",express.static(path.join(process.cwd(), "public", "uploads"))
 app.use("/convert",conversionRoutes);
 app.use("/status", statusRoutes);
 app.use('/download', downloadRoutes);
+app.use(errorHandler);
 export default app;
